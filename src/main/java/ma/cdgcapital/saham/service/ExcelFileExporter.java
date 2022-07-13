e7e094f59204303154cb65f8c20528ea128c70e5
hamzak97 <oukhyadhamza@gmail.com>
2022-07-13 23:11:27 +0100

    last commit

--- src/main/java/ma/cdgcapital/saham/service/ExcelFileExporter.java
+++ src/main/java/ma/cdgcapital/saham/service/ExcelFileExporter.java
@@ -2,10 +2,12 @@ package ma.cdgcapital.saham.service;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import ma.cdgcapital.saham.model.Compte;
+import ma.cdgcapital.saham.model.Operation;
 import ma.cdgcapital.saham.model.Position;
 import ma.cdgcapital.saham.model.XlsColumn;
 import ma.cdgcapital.saham.model.provider.CompteTitreProvider;
@@ -45,18 +47,23 @@ public class ExcelFileExporter {
 
   private static final List<XlsColumn> positionColumns =
       Arrays.asList(
-          new XlsColumn("DATEEXTRATION", 1),
+          new XlsColumn("DATE EXTRATION", 1),
           new XlsColumn("IDCLIENT", 1),
-          new XlsColumn("CLIENTLIBELLE", 1),
-          new XlsColumn("COMPTETITRE", 1),
-          new XlsColumn("COMPTESESPECES", 1),
+          new XlsColumn("CLIENT LIBELLE", 1),
+          new XlsColumn("COMPTE TITRE", 1),
+          new XlsColumn("COMPTES ESPECES", 1),
           new XlsColumn("ISIN", 1),
-          new XlsColumn("ISINDESCRIPTION", 1),
+          new XlsColumn("ISIN DESCRIPTION", 1),
           new XlsColumn("QUANTITE", 1),
-          new XlsColumn("PRIXUNITAIRE", 1),
-          new XlsColumn("VALORISATIONPOSITION", 1),
-          new XlsColumn("NATUREPOSITION", 1));
+          new XlsColumn("PRIX UNITAIRE", 1),
+          new XlsColumn("VALORISATION POSITION", 1),
+          new XlsColumn("NATURE POSITION", 1));
 
+  private static final List<XlsColumn> pos =
+	      Arrays.asList(
+	          );
+
+  
   public static HSSFFont generateFont(
       HSSFWorkbook wb, short bold, short color, String fontName, short fontHeight) {
     HSSFFont font = wb.createFont();
@@ -120,6 +127,8 @@ public class ExcelFileExporter {
 
     List<Position> positions = compteTitreProvider.getPositions("1410000199509");
     Compte compte = compteTitreProvider.getCompteTitre("1410000199509");
+    
+   
     HSSFWorkbook wb = new HSSFWorkbook();
     HSSFSheet sheet = wb.createSheet("Feuil1");
 
@@ -160,5 +169,87 @@ public class ExcelFileExporter {
     out.close();
   }
 
-  public static void generateReleveChronoOperationsTitres() {}
+  public static void generateReleveChronoOperationsTitres() throws IOException {
+	  Operation operation = compteTitreProvider.getReleveChronologiques("1410000199509");
+	  
+	  
+	  HSSFWorkbook wb = new HSSFWorkbook();
+	    HSSFSheet sheet = wb.createSheet("SMC");
+
+	    HSSFFont fontTitle =
+	        generateFont(
+	            wb, HSSFFont.BOLDWEIGHT_BOLD, IndexedColors.WHITE.getIndex(), "Calibri", (short) 11);
+	    
+	    HSSFFont fontTitle1 =
+		        generateFont(
+		            wb, HSSFFont.BOLDWEIGHT_BOLD, IndexedColors.BLUE.getIndex(), "Calibri", (short) 11);
+	    createRow(
+	        sheet,
+	       null,
+	        cellStyle(wb, fontTitle, IndexedColors.GREY_50_PERCENT.index),
+	        3,
+	        3,
+	        2,
+	        14,
+	        parentColumns);
+	  
+	    
+	    createRow(
+		        sheet,
+		        null,
+		        cellStyle(wb, fontTitle, IndexedColors.GREY_50_PERCENT.index),
+		        4,
+		        4,
+		        2,
+		        14,
+		        childColumns);
+	
+	    
+		    createRow(
+			        sheet,
+			        "Relevé chronologique des opérations Titres",
+			        cellStyle(wb, fontTitle1, IndexedColors.WHITE.index),
+			        0,
+			        0,
+			        2,
+			        9,
+			        pos);
+			
+		    
+
+			    createRow(
+				        sheet,
+				        "Du : 01/01/2018 Au  11/06/2021  SMC",
+				        cellStyle(wb, fontTitle1, IndexedColors.WHITE.index),
+				        1,
+				        2,
+				        1,
+				        9,
+				        pos);
+			sheet.setDisplayGridlines(false);
+				    
+			System.out.print(operation.getContent().size());
+			
+			
+			 int rowNum = 5;
+			    for (int i = 0; i < operation.getContent().size(); i++) {
+			      sheet.autoSizeColumn((short) i);
+			      HSSFRow row = sheet.createRow(rowNum++);
+			      row.createCell((short) 2).setCellValue(todayAmountTwo());
+			      row.createCell((short) 3).setCellValue(operation.getContent().get(i).getDateOperation());
+			      row.createCell((short) 4).setCellValue(operation.getContent().get(i).getReferenceCro());
+			      row.createCell((short) 5).setCellValue(operation.getContent().get(i).getLibelle());
+			      row.createCell((short) 6).setCellValue(operation.getContent().get(i).getCodeIsin());
+			      row.createCell((short) 7).setCellValue(operation.getContent().get(i).getDescriptionTitre());
+			      row.createCell((short) 8).setCellValue(operation.getContent().get(i).getQuantite());
+			      row.createCell((short) 10).setCellValue(operation.getContent().get(i).getMontantBrut());
+			      row.createCell((short) 11).setCellValue(operation.getContent().get(i).getMontantNet());
+			      
+			    }
+
+	    FileOutputStream out = new FileOutputStream("SMC.xls");
+	    wb.write(out);
+	    out.close();
+	    
+  }
 }
